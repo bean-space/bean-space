0925bab8d20164b18fab60362434837b2da2c077
sangeuuun <151465485+sangeuuun@users.noreply.github.com>
2024-07-17 11:32:08 +0900

    :sparkles: 회원 목록 조회, 공간 관리 기능 구현 (#33)
    Co-authored-by: sangeuuun <nwm21d@kakao.com>

--- src/main/kotlin/com/beanspace/beanspace/api/admin/AdminService.kt
+++ src/main/kotlin/com/beanspace/beanspace/api/admin/AdminService.kt
@@ -1,38 +1,80 @@
 package com.beanspace.beanspace.api.admin
 
 import com.beanspace.beanspace.api.admin.dto.RequestAddSpaceResponse
+import com.beanspace.beanspace.api.admin.dto.UpdateSpaceStatus
 import com.beanspace.beanspace.api.coupon.dto.CouponResponse
+import com.beanspace.beanspace.api.member.dto.MemberListResponse
 import com.beanspace.beanspace.domain.coupon.repository.CouponRepository
+import com.beanspace.beanspace.domain.exception.ModelNotFoundException
+import com.beanspace.beanspace.domain.member.repository.MemberRepository
+import com.beanspace.beanspace.domain.space.model.SpaceStatus
+import com.beanspace.beanspace.domain.space.repository.SpaceRepository
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class AdminService(
-    private val couponRepository: CouponRepository,
+    private val spaceRepository: SpaceRepository,
+    private val memberRepository: MemberRepository,
+    private val couponRepository: CouponRepository
 ) {
-    fun getRequestAddSpace(): List<RequestAddSpaceResponse> {
-        TODO()
+    fun getRequestAddSpace(pageable: Pageable, status: String): Page<RequestAddSpaceResponse> {
+        // 권한 확인 추가
+        val spaceStatus = when (status) {
+            "PENDING" -> SpaceStatus.PENDING
+            "REJECTED" -> SpaceStatus.REJECTED
+            "ACTIVE" -> SpaceStatus.ACTIVE
+            else -> throw IllegalArgumentException("해당하는 상태의 요청이 없습니다.")
+        }
+        return spaceRepository.findByStatus(pageable, spaceStatus).map { RequestAddSpaceResponse.from(it) }
     }
 
-    fun modifySpaceStatus(spaceId: Long) {
-        //TODO
+    fun updateSpaceStatus(spaceId: Long, request: UpdateSpaceStatus) {
+        val space = spaceRepository.findByIdOrNull(spaceId) ?: throw ModelNotFoundException("Space", spaceId)
+
+        if (space.isProceeded())
+            throw IllegalArgumentException("이미 변경한 요청입니다.")
+
+        when (request.status) {
+            SpaceStatus.ACTIVE.name -> {
+                space.active()
+                spaceRepository.save(space)
+            }
+
+            SpaceStatus.REJECTED.name -> {
+                space.reject()
+                spaceRepository.save(space)
+            }
+
+            else -> {
+                throw IllegalArgumentException("유효하지 않은 요청 입니다.")
+            }
+        }
     }
 
-    fun getMemberList() {
-        TODO()
+    fun getMemberList(): List<MemberListResponse> {
+        return memberRepository.findAll().map { MemberListResponse.from(it) }
     }
 
     fun getCouponList(): List<CouponResponse> {
+        // 권한 확인 추가
         return couponRepository.findAll().map { CouponResponse.from(it) }
     }
 
+    @Transactional
     fun createCoupon(): CouponResponse {
         TODO()
     }
 
+    @Transactional
     fun updateCoupon(couponId: Long): CouponResponse {
         TODO()
     }
 
+    @Transactional
     fun deleteCoupon(couponId: Long) {
         TODO()
     }
--- src/main/kotlin/com/beanspace/beanspace/domain/space/repository/SpaceQueryDslRepository.kt
+++ src/main/kotlin/com/beanspace/beanspace/domain/space/repository/SpaceQueryDslRepository.kt
@@ -1,3 +1,10 @@
 package com.beanspace.beanspace.domain.space.repository
 
-interface SpaceQueryDslRepository
+import com.beanspace.beanspace.domain.space.model.Space
+import com.beanspace.beanspace.domain.space.model.SpaceStatus
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+
+interface SpaceQueryDslRepository {
+    fun findByStatus(pageable: Pageable, spaceStatus: SpaceStatus): Page<Space>
+}
