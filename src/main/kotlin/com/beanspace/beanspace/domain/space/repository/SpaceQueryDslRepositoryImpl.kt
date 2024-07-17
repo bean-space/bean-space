aa27f81454634b47a31760134fb65d29abb1f59f
sangeuuun <nwm21d@kakao.com>
2024-07-17 11:13:50 +0900

    :sparkles: 회원 목록 조회, 공간 관리 기능 구현

--- src/main/kotlin/com/beanspace/beanspace/domain/space/repository/SpaceQueryDslRepositoryImpl.kt
+++ src/main/kotlin/com/beanspace/beanspace/domain/space/repository/SpaceQueryDslRepositoryImpl.kt
@@ -1,9 +1,54 @@
 package com.beanspace.beanspace.domain.space.repository
 
+import com.beanspace.beanspace.domain.space.model.QSpace
+import com.beanspace.beanspace.domain.space.model.Space
+import com.beanspace.beanspace.domain.space.model.SpaceStatus
+import com.querydsl.core.BooleanBuilder
+import com.querydsl.core.types.Expression
+import com.querydsl.core.types.Order
+import com.querydsl.core.types.OrderSpecifier
+import com.querydsl.core.types.dsl.EntityPathBase
+import com.querydsl.core.types.dsl.PathBuilder
 import com.querydsl.jpa.impl.JPAQueryFactory
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageImpl
+import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Repository
 
 @Repository
 class SpaceQueryDslRepositoryImpl(
     private val queryFactory: JPAQueryFactory
-) : SpaceQueryDslRepository
+) : SpaceQueryDslRepository {
+
+    private val space = QSpace.space
+
+    override fun findByStatus(pageable: Pageable, spaceStatus: SpaceStatus): Page<Space> {
+        val whereClause = BooleanBuilder()
+        whereClause.and(space.status.eq(spaceStatus))
+
+        val totalCount = queryFactory.select(space.count())
+            .from(space)
+            .where(whereClause)
+            .fetchOne() ?: 0L
+
+        val contents = queryFactory.selectFrom(space)
+            .where(whereClause)
+            .offset(pageable.offset)
+            .limit(pageable.pageSize.toLong())
+            .orderBy(*getOrderSpecifier(pageable, space))
+            .fetch()
+
+        return PageImpl(contents, pageable, totalCount)
+    }
+
+    private fun getOrderSpecifier(pageable: Pageable, path: EntityPathBase<*>): Array<OrderSpecifier<*>> {
+        val pathBuilder = PathBuilder(path.type, path.metadata)
+
+        return pageable.sort.toList().map { order ->
+            OrderSpecifier(
+                if (order.isAscending) Order.ASC else Order.DESC,
+                pathBuilder.get(order.property) as Expression<Comparable<*>>
+            )
+        }.toTypedArray()
+    }
+}
