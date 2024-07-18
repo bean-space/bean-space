5c5d7f256144540b49022dee8467e817df0dbf3c
DanDanjoo <murekal94@gmail.com>
2024-07-18 09:44:34 +0900

    :sparkles: 회원가입 기능 구현 (#34)
    * 🛂 인증 및 jwt 관련 기본 정의
    
    - SecurityConfig : 기본 HTTP 보안 설정
    - JwtPlugin : token 생성 및 검증 설정
    - JwtAuthenticationFilter : HTTP 요청에서 JWT 토큰을 처리하도록 JwtAuthenticationFilter 생성
    - UserPrincipal 인증된 사용자를 나타내는 DTO 추가
    
    * :sparkles: 인증을 위한 기능 구현
    
    - UserPrincipal : 인증된 사용자의 정보 email, role 변경
    - JwtAuthenticationToken : JWT 기반 인증에서 로그인한 사용자의 정보를 담고 있는 객체
    - JwtAuthenticationFilter : 현재 필터에서 요청과 응답을 처리하고, 그 후에 다음 필터로 제어를 전달하는 역할을 하는 doFilter 추가
    - SecurityConfig : 클래스에 jwtAuthenticationFilter 의존성 추가
    
    * :sparkles: 회원가입 기능 구현
    
    - AuthController : AuthService 생성자 주입 및 service 연결
    - AuthService : MemberRepository 생성자 주입 및 Email 로그인 방식 테스트중
    - MemberRepository : 이메일로 멤버 검색 및 존재 여부 확인 메서드 추가 existsByEmail, findByEmail
    - JwtPlugin, yml : yml 파일 시크릿 추가 및 @Value 파라미터 변경, JWT 관련 설정을 외부 yml 파일로부터 읽어오도록 변경
    
    * :sparkles: 비밀번호 암호화 기능 추가
    
    - PasswordEncoderConfig : DB에 비밀번호 암호화 저장 기능 구현
    - AuthService : passwordEncoder 생성자 주입 및 비밀번호, email에 대한 예외처리 추가
    - SignUpRequest : 비밀번호 확인 필드 추가 및 PasswordEncoderConfig 생성자 주입
    - SignUpResponse : 전화번호, 역할 필드 추가
    
    * :sparkles: 접근 권한에 대한 CustomException 추가
    
    - GlobalExceptionHandler : 공통 ExceptionHandle에 AccessDeniedException 추가
    
    * :sparkles: 회원가입에 필요한 validation 추가
    
    - AuthController : @Valid 어노테이션 추가
    - SignUpRequest : 이메일, 비밀번호, 닉네임, 전화번호 validation 추가
    - JwtPlugin : username -> email로 변경
    - SwaggerConfig : SwaggerConfig 클래스 생성
    - SecurityConfig : h2 연결을 위한 로직 추가
    - build.gradle : validation 의존성 추가
    - application.yml : h2 연결을 위해 yml 변경
    
    * :sparkles: 인증, 인가 예외처리 추가
    
    - CustomAccessDeniedHandler
    - CustomAuthenticationEntrypoint
    
    * ✏️ 사용하지 않는 함수 제거
    
    * ✏️ JwtPlugin 토큰 생성 메서드 일부 수정
    
    - generateAccessToken, generateToken email 정보 삭제
    
    * ✏️ SignUpRequest 필드 변경
    
    - phoneNumber 번호에 대한 validation 수정
    
    * ✏️ SignUpRequest 메시지 변경
    
    - phoneNumber 번호에 대한 validation message 수정

--- src/main/kotlin/com/beanspace/beanspace/domain/member/repository/MemberRepository.kt
+++ src/main/kotlin/com/beanspace/beanspace/domain/member/repository/MemberRepository.kt
@@ -3,4 +3,9 @@ package com.beanspace.beanspace.domain.member.repository
 import com.beanspace.beanspace.domain.member.model.Member
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface MemberRepository : JpaRepository<Member, Long>
+interface MemberRepository : JpaRepository<Member, Long> {
+
+    fun findByEmail(email: String): Member
+
+    fun existsByEmail(email: String): Boolean
+}
---
+++ src/main/kotlin/com/beanspace/beanspace/infra/security/jwt/JwtPlugin.kt
@@ -0,0 +1,53 @@
+package com.beanspace.beanspace.infra.security.jwt
+
+import io.jsonwebtoken.Claims
+import io.jsonwebtoken.Jws
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.security.Keys
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import java.nio.charset.StandardCharsets
+import java.util.*
+
+@Component
+class JwtPlugin(
+    @Value("\${auth.jwt.issuer}") private val issuer: String,
+    @Value("\${auth.jwt.secret}") private val secret: String,
+    @Value("\${auth.jwt.accessTokenExpirationHour}") private val accessTokenExpirationHour: Long
+) {
+    var now = Date()
+    val accessTokenExpiration = Date(now.time + accessTokenExpirationHour * 1000 * 1000)
+    val key = Keys.hmacShaKeyFor(secret.toByteArray(StandardCharsets.UTF_8))
+
+    fun validateToken(jwt: String): Result<Jws<Claims>> {
+        return kotlin.runCatching {
+            Jwts.parser().verifyWith(key).build().parseSignedClaims(jwt)
+        }
+    }
+
+    fun generateAccessToken(
+        subject: String,
+        role: String
+    ): String {
+        return generateToken(subject, role, accessTokenExpiration)
+    }
+
+    private fun generateToken(
+        subject: String,
+        role: String,
+        accessTokenExpiration: Date
+    ): String {
+        val claims: Claims = Jwts.claims()
+            .add(mapOf("role" to role))
+            .build()
+
+        return Jwts.builder()
+            .subject(subject)
+            .issuer(issuer)
+            .issuedAt(now)
+            .expiration(accessTokenExpiration)
+            .claims(claims)
+            .signWith(key)
+            .compact()
+    }
+}
