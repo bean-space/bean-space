b52aa42fa9c646d215d6dfc2a43ade98465cbb1d
DanDanjoo <murekal94@gmail.com>
2024-07-18 09:41:23 +0900

    ✏️ SignUpRequest 메시지 변경
    - phoneNumber 번호에 대한 validation message 수정

--- src/main/kotlin/com/beanspace/beanspace/api/auth/dto/SignUpRequest.kt
+++ src/main/kotlin/com/beanspace/beanspace/api/auth/dto/SignUpRequest.kt
@@ -33,10 +33,10 @@ data class SignUpRequest(
     )
     val nickname: String,
 
-    @field:NotBlank(message = "전화번호를 입력해주세요.")
+    @field:NotBlank(message = "휴대폰 번호를 입력해주세요.")
     @field:Pattern(
         regexp = "^010\\d{8}\$",
-        message = "전화번호는 숫자 11자로 설정해야합니다."
+        message = "휴대폰 번호는 010으로 시작해서 11자로 설정해야합니다."
     )
     val phoneNumber: String
 
