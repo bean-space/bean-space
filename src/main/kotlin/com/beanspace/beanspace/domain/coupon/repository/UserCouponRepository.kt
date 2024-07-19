6c62d984ac233b995b7cf3f18ba56fec673618dd
moomoo <anwns1211@gmail.com>
2024-07-19 12:27:37 +0900

    :fire: 불필요한 클래스, 함수 삭제

--- src/main/kotlin/com/beanspace/beanspace/domain/coupon/repository/UserCouponRepository.kt
+++ src/main/kotlin/com/beanspace/beanspace/domain/coupon/repository/UserCouponRepository.kt
@@ -3,6 +3,4 @@ package com.beanspace.beanspace.domain.coupon.repository
 import com.beanspace.beanspace.domain.coupon.model.UserCoupon
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface UserCouponRepository : JpaRepository<UserCoupon, Long> {
-    fun findAllByMemberId(memberId: Long): List<UserCoupon>
-}
+interface UserCouponRepository : JpaRepository<UserCoupon, Long>
