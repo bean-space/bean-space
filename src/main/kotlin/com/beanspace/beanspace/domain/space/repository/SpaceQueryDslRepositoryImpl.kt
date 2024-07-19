a091a6f02cda4d052cc2751b16357f032c45095c
itssu <63135789+devitssu@users.noreply.github.com>
2024-07-19 15:06:03 +0900

    ✨ 공간 CRUD 구현 (#43)
    * 🚚 AccessDeniedException.kt -> NoPermissionException.kt 으로 이름 변경
    
    * 🔧 SecurityConfig @EnableMethodSecurity 어노테이션 추가
    - @PreAuthorize 사용하기 위해
    
    * ✨ [HOST] Space 생성 기능 구현
    
    * ✨ [HOST] Space 수정 기능 구현
    
    * ✨ [HOST] Space 삭제 기능 구현
    
    * ✨ Space 단건 조회 기능 구현
     - 추후 예약불가능 날짜 목록 반환 추가
    
    * ✨ Space 목록 조회 기능 구현
    - 페이징 적용
    - 검색 적용
    
    * ✅ http 테스트용 파일 추가
    
    * 🐛 인원으로 검색시 조건 수정
     - 기본인원 고려 x, 최대인원만 비교

--- src/main/kotlin/com/beanspace/beanspace/domain/space/repository/SpaceQueryDslRepositoryImpl.kt
+++ src/main/kotlin/com/beanspace/beanspace/domain/space/repository/SpaceQueryDslRepositoryImpl.kt
@@ -1,19 +1,27 @@
 package com.beanspace.beanspace.domain.space.repository
 
+import com.beanspace.beanspace.domain.image.model.ImageType
+import com.beanspace.beanspace.domain.image.model.QImage
+import com.beanspace.beanspace.domain.reservation.model.QReservation
 import com.beanspace.beanspace.domain.space.model.QSpace
 import com.beanspace.beanspace.domain.space.model.Space
 import com.beanspace.beanspace.domain.space.model.SpaceStatus
 import com.querydsl.core.BooleanBuilder
+import com.querydsl.core.Tuple
 import com.querydsl.core.types.Expression
 import com.querydsl.core.types.Order
 import com.querydsl.core.types.OrderSpecifier
+import com.querydsl.core.types.dsl.BooleanExpression
 import com.querydsl.core.types.dsl.EntityPathBase
 import com.querydsl.core.types.dsl.PathBuilder
+import com.querydsl.core.util.StringUtils
+import com.querydsl.jpa.JPAExpressions
 import com.querydsl.jpa.impl.JPAQueryFactory
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageImpl
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Repository
+import java.time.LocalDate
 
 @Repository
 class SpaceQueryDslRepositoryImpl(
@@ -21,24 +29,95 @@ class SpaceQueryDslRepositoryImpl(
 ) : SpaceQueryDslRepository {
 
     private val space = QSpace.space
+    private val reservation = QReservation.reservation
+    private val image = QImage.image
 
     override fun findByStatus(pageable: Pageable, spaceStatus: SpaceStatus): Page<Space> {
-        val whereClause = BooleanBuilder()
-        whereClause.and(space.status.eq(spaceStatus))
-
         val totalCount = queryFactory.select(space.count())
             .from(space)
-            .where(whereClause)
+            .where(eqStatus(spaceStatus))
             .fetchOne() ?: 0L
 
         val contents = queryFactory.selectFrom(space)
-            .where(whereClause)
+            .where(eqStatus(spaceStatus))
             .offset(pageable.offset)
             .limit(pageable.pageSize.toLong())
             .orderBy(*getOrderSpecifier(pageable, space))
             .fetch()
 
         return PageImpl(contents, pageable, totalCount)
+    }
+
+    override fun search(
+        sido: String?,
+        checkIn: LocalDate?,
+        checkOut: LocalDate?,
+        headCount: Int?,
+        pageable: Pageable
+    ): Pair<Map<Space?, List<String>>, Long> {
+
+        val conditions = BooleanBuilder()
+            .and(eqStatus(SpaceStatus.ACTIVE))
+            .and(eqSido(sido))
+            .and(inAvailableDate(checkIn, checkOut))
+            .and(isAvailableHeadCount(headCount))
+
+        val totalCount = queryFactory.select(space.count())
+            .from(space)
+            .where(conditions)
+            .fetchOne() ?: return Pair(emptyMap(), 0)
+
+        val paginatedSpaceId = queryFactory
+            .select(space.id).distinct()
+            .from(space)
+            .where(conditions)
+            .offset(pageable.offset)
+            .limit(pageable.pageSize.toLong())
+            .orderBy(*getOrderSpecifier(pageable, space))
+            .fetch()
+
+        val result = queryFactory.select(space, image.imageUrl)
+            .from(space)
+            .leftJoin(image).on(image.contentId.eq(space.id).and(image.type.eq(ImageType.SPACE)))
+            .where(space.id.`in`(paginatedSpaceId))
+            .orderBy(*getOrderSpecifier(pageable, space))
+            .fetch()
+
+        val contents = result.groupBy { it.get(QSpace.space) }
+            .mapKeys { (space, _) -> space }
+            .mapValues { it.value.map { tuple -> tuple.get(QImage.image.imageUrl) ?: "" } }
+
+        return Pair(contents, totalCount)
+    }
+
+    override fun getReservedDateList(spaceId: Long): MutableList<Tuple>? {
+        return queryFactory.select(reservation.checkIn, reservation.checkOut)
+            .from(reservation)
+            .join(space).on(reservation.space.id.eq(spaceId))
+            .fetch()
+    }
+
+    private fun isAvailableHeadCount(headCount: Int?): BooleanExpression? {
+        return if (headCount != null && headCount > 0) space.maxPeople.goe(headCount) else null
+    }
+
+    private fun inAvailableDate(start: LocalDate?, end: LocalDate?): BooleanExpression? {
+        val startCondition =
+            if (start != null) reservation.checkIn.loe(start).and(reservation.checkOut.gt(start)) else null
+        val endCondition = if (end != null) reservation.checkIn.lt(end).and(reservation.checkOut.goe(end)) else null
+
+        val unavailableSpaceIds = JPAExpressions.select(reservation.space.id)
+            .from(reservation)
+            .where(BooleanBuilder().and(startCondition).or(endCondition))
+        return space.id.notIn(unavailableSpaceIds)
+    }
+
+    private fun eqStatus(spaceStatus: SpaceStatus?): BooleanExpression? {
+        return if (spaceStatus != null) space.status.eq(spaceStatus) else null
+    }
+
+    private fun eqSido(sido: String?): BooleanExpression? {
+        return if (!StringUtils.isNullOrEmpty(sido)) space.address.sido.eq(sido) else null
     }
 
     private fun getOrderSpecifier(pageable: Pageable, path: EntityPathBase<*>): Array<OrderSpecifier<*>> {
--- src/main/kotlin/com/beanspace/beanspace/infra/security/config/SecurityConfig.kt
+++ src/main/kotlin/com/beanspace/beanspace/infra/security/config/SecurityConfig.kt
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.security.servlet.PathRequest
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.SecurityFilterChain
@@ -14,6 +15,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 class SecurityConfig(
     private val jwtAuthenticationFilter: JwtAuthenticationFilter,
     private val customAccessDeniedHandler: CustomAccessDeniedHandler,
