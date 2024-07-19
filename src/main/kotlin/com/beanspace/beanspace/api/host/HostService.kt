495d0e8d469f59771b107fc21ce47eab9d1df21a
itssu <ssujinn96@gmail.com>
2024-07-19 11:55:27 +0900

    ✨ [HOST] Space 생성 기능 구현

--- src/main/kotlin/com/beanspace/beanspace/api/host/HostService.kt
+++ src/main/kotlin/com/beanspace/beanspace/api/host/HostService.kt
@@ -1,16 +1,45 @@
 package com.beanspace.beanspace.api.host
 
+import com.beanspace.beanspace.api.host.dto.AddSpaceRequest
+import com.beanspace.beanspace.api.host.dto.UpdateSpaceRequest
 import com.beanspace.beanspace.api.space.dto.SpaceResponse
+import com.beanspace.beanspace.domain.exception.ModelNotFoundException
+import com.beanspace.beanspace.domain.exception.NoPermissionException
+import com.beanspace.beanspace.domain.image.model.Image
+import com.beanspace.beanspace.domain.image.model.ImageType
+import com.beanspace.beanspace.domain.image.repository.ImageRepository
+import com.beanspace.beanspace.domain.member.repository.MemberRepository
 import com.beanspace.beanspace.domain.space.repository.SpaceRepository
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
-class HostService(private val spaceRepository: SpaceRepository) {
+class HostService(
+    private val spaceRepository: SpaceRepository,
+    private val memberRepository: MemberRepository,
+    private val imageRepository: ImageRepository,
+) {
 
     @Transactional
-    fun createSpace(): SpaceResponse {
-        TODO("Not yet implemented")
+    fun createSpace(request: AddSpaceRequest, hostId: Long): SpaceResponse {
+        val host = memberRepository.findByIdOrNull(hostId) ?: throw ModelNotFoundException(model = "Host", id = hostId)
+        val savedSpace = spaceRepository.save(AddSpaceRequest.toEntity(request, host))
+
+        val imageUrlList = request.imageUrlList
+
+        request.imageUrlList.forEachIndexed { index, imageUrl ->
+            imageRepository.save(
+                Image(
+                    type = ImageType.SPACE,
+                    contentId = savedSpace.id!!,
+                    imageUrl = imageUrl,
+                    orderIndex = index
+                )
+            )
+        }
+
+        return SpaceResponse.from(savedSpace, imageUrlList)
     }
 
     @Transactional
--- src/main/kotlin/com/beanspace/beanspace/domain/space/model/Space.kt
+++ src/main/kotlin/com/beanspace/beanspace/domain/space/model/Space.kt
@@ -1,16 +1,19 @@
 package com.beanspace.beanspace.domain.space.model
 
+import com.beanspace.beanspace.domain.common.BaseTimeEntity
 import com.beanspace.beanspace.domain.member.model.Member
 import jakarta.persistence.Column
 import jakarta.persistence.Embedded
 import jakarta.persistence.Entity
 import jakarta.persistence.EnumType
 import jakarta.persistence.Enumerated
+import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.JoinColumn
 import jakarta.persistence.ManyToOne
+import java.time.LocalDateTime
 
 @Entity
 class Space(
@@ -48,14 +51,20 @@ class Space(
     @Enumerated(EnumType.STRING)
     var status: SpaceStatus = SpaceStatus.PENDING,
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "host_id")
     val host: Member,
+
+    @Column
+    var isDeleted: Boolean = false,
+
+    @Column
+    var deletedAt: LocalDateTime? = null,
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null,
-) {
+) : BaseTimeEntity() {
     fun isProceeded(): Boolean {
         return status != SpaceStatus.PENDING
     }
