a43cc438a647be5d2dd4c3d755d5b35cae8a1c18
itssu <ssujinn96@gmail.com>
2024-07-23 23:20:02 +0900

    ➕테스트 관련 의존성 추가 - kotest-extensions-spring - kotlinfixture

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,7 +55,10 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
     testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.1")
     testImplementation("org.springframework.security:spring-security-test")
+    testImplementation("com.appmattus.fixture:fixture:1.2.0")
+    testImplementation("com.appmattus.fixture:fixture-kotest:1.2.0")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
