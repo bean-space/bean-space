0075354d21dd7c08619f90f223f1f0c539b331a8
itssu <ssujinn96@gmail.com>
2024-08-04 21:07:33 +0900

    ✅ redis 테스트시 testcontainer 사용

--- src/main/kotlin/com/beanspace/beanspace/infra/redis/RedisConfig.kt
+++ src/main/kotlin/com/beanspace/beanspace/infra/redis/RedisConfig.kt
@@ -6,11 +6,13 @@ import org.redisson.config.Config
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.data.redis.serializer.StringRedisSerializer
 
+@Profile("!test")
 @Configuration
 class RedisConfig(
     @Value("\${spring.data.redis.host}") private val host: String,
@@ -36,7 +38,7 @@ class RedisConfig(
     @Bean
     fun redissonClient(): RedissonClient {
         val config = Config()
-        config.useSingleServer().address = "redis://localhost:6379"
+        config.useSingleServer().address = "redis://${host}:${port}"
         return Redisson.create(config)
     }
 }
