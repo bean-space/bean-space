961f1cc1098c4da8a9acc37a6f02ad5eaf282872
moomoo <anwns1211@gmail.com>
2024-08-05 00:23:43 +0900

    :sparkles: CouponResponse 발급 시작일 기준으로 정렬되도록 수정

--- src/main/kotlin/com/beanspace/beanspace/domain/coupon/repository/CouponRepository.kt
+++ src/main/kotlin/com/beanspace/beanspace/domain/coupon/repository/CouponRepository.kt
@@ -3,4 +3,6 @@ package com.beanspace.beanspace.domain.coupon.repository
 import com.beanspace.beanspace.domain.coupon.model.Coupon
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface CouponRepository : JpaRepository<Coupon, Long>, CustomCouponRepository
+interface CouponRepository : JpaRepository<Coupon, Long>, CustomCouponRepository {
+    fun findAllByOrderByIssueStartAtDesc(): List<Coupon>
+}
