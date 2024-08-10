626c7026493343b5cb4cbfc24b80c0db5c4008f5
moomoo <122208574+Moo-moo-11@users.noreply.github.com>
2024-08-10 17:40:11 +0900

    :sparkles: 프로필 수정, 소셜 로그인 유저 정보 추가시 받는 정보 수정 (#131)

--- src/main/kotlin/com/beanspace/beanspace/api/member/dto/UpdateSocialUserInfoRequest.kt
+++ src/main/kotlin/com/beanspace/beanspace/api/member/dto/UpdateSocialUserInfoRequest.kt
@@ -9,11 +9,5 @@ data class UpdateSocialUserInfoRequest(
         regexp = "^010\\d{8}\$",
         message = "휴대폰 번호는 010으로 시작해서 11자로 설정해야합니다."
     )
-    val phoneNumber: String,
-    @field:NotBlank(message = "이메일을 입력해주세요.")
-    @field:Pattern(
-        regexp = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$",
-        message = "올바른 이메일 형식을 입력해주세요."
-    )
-    val email: String
+    val phoneNumber: String
 )
