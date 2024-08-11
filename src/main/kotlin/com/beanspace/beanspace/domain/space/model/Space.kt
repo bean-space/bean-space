4b41f13ee21609e63c1beaf9ded545a697435a08
moomoo <anwns1211@gmail.com>
2024-08-11 14:20:29 +0900

    🐛 삭제된 공간이 조회되는 버그 수정

--- src/main/kotlin/com/beanspace/beanspace/domain/space/model/Space.kt
+++ src/main/kotlin/com/beanspace/beanspace/domain/space/model/Space.kt
@@ -13,9 +13,11 @@ import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.JoinColumn
 import jakarta.persistence.ManyToOne
+import org.hibernate.annotations.SQLRestriction
 import java.time.LocalDateTime
 
 @Entity
+@SQLRestriction("is_deleted = false")
 class Space(
     @Column
     var listingName: String,
